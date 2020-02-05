9ff568099883966f6f0d4f5013f3355ef55489fc
taneishi <taneishi@users.noreply.github.com>
2020-02-05 09:26:30 +0900

    all data on device

--- autoencoder.py
+++ autoencoder.py
@@ -8,10 +8,10 @@ from torch.utils.data import TensorDataset
 import matplotlib.pyplot as plt
 
 def show_torch_image(torch_tensor, name):
-    plt.imshow(torch_tensor.numpy().reshape(28, 28), cmap='gray')
+    plt.imshow(torch_tensor.cpu().reshape(28, 28), cmap='gray')
     plt.savefig('figure/%s.png' % name)
 
-def load_dataset(batch_size):
+def load_dataset(batch_size, device):
     # Load dataset
     train = np.load('data/fashion-mnist_train.npz', allow_pickle=True)['data']
 
@@ -26,17 +26,17 @@ def load_dataset(batch_size):
     print('train %d test %d' % (len(train_y), len(valid_y)))
 
     # create torch tensor from numpy array
-    train_x_torch = torch.from_numpy(train_x).type(torch.FloatTensor)
-    train_y_torch = torch.from_numpy(train_y)
+    train_x_torch = torch.FloatTensor(train_x).to(device)
+    train_y_torch = torch.ShortTensor(train_y).to(device)
 
-    valid_x_torch = torch.from_numpy(valid_x).type(torch.FloatTensor)
-    valid_y_torch = torch.from_numpy(valid_y)
+    valid_x_torch = torch.FloatTensor(valid_x).to(device)
+    valid_y_torch = torch.ShortTensor(valid_y).to(device)
 
     train = TensorDataset(train_x_torch, train_y_torch)
     valid = TensorDataset(valid_x_torch, valid_y_torch)
 
-    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4)
-    valid_dataloader = torch.utils.data.DataLoader(valid, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
+    valid_dataloader = torch.utils.data.DataLoader(valid, batch_size=batch_size, shuffle=False)
 
     show_torch_image(train_x_torch[1], 'train_sample')
     show_torch_image(valid_x_torch[1], 'valid_sample')
@@ -73,14 +73,13 @@ class AutoEncoder(nn.Module):
         x = self.output_layer(x)
         return x
 
-def train(dataloader, model, optimizer, loss_func, batch_size, device):
+def train(dataloader, model, optimizer, loss_func, batch_size):
     model.train()
     train_loss = 0
     EPOCHS = 10
 
     for epoch in range(EPOCHS):
         for index, (data, target) in enumerate(dataloader, 1):
-            data = data.to(device)
             optimizer.zero_grad()
             pred = model(data)
             loss = loss_func(pred, data)
@@ -90,12 +89,11 @@ def train(dataloader, model, optimizer, loss_func, batch_size, device):
             print('\repoch: %2d [%3d/%3d] train_loss: %5.3f' % (epoch, index, len(dataloader), loss.item()), end='')
         print('')
 
-def test(dataloader, model, device):
+def test(dataloader, model):
     model.eval()
     predictions = []
 
     for data, target in dataloader:
-            data = data.to(device)
             pred = model(data)
             
             for prediction in pred:
@@ -109,7 +107,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using %s device.' % device)
 
-    train_dataloader, valid_dataloader = load_dataset(batch_size)
+    train_dataloader, valid_dataloader = load_dataset(batch_size, device)
 
     ae = AutoEncoder().to(device)
     print(ae)
@@ -118,9 +116,9 @@ def main():
     loss_func = nn.MSELoss()
     optimizer = torch.optim.Adam(ae.parameters(), lr=1e-3)
 
-    train(train_dataloader, ae, optimizer, loss_func, batch_size, device)
+    train(train_dataloader, ae, optimizer, loss_func, batch_size)
 
-    predictions = test(valid_dataloader, ae, device)
+    predictions = test(valid_dataloader, ae)
     print(len(predictions))
 
     show_torch_image(predictions[1].cpu().detach(), 'pred_sample')
