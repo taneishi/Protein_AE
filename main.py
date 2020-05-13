216c1798a9312679c929c8a863ffa7c7bf6305ba
taneishi <taneishi@users.noreply.github.com>
2020-05-13 23:06:43 +0900

    test at epoch end

--- main.py
+++ main.py
@@ -36,55 +36,62 @@ class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
         # encoder
-        self.e1 = nn.Linear(2250, 1000)
-        self.e2 = nn.Linear(1000, 500)
-        self.e3 = nn.Linear(500, 250)
+        self.e1 = nn.Linear(1900, 1600)
+        self.e2 = nn.Linear(1600, 1300)
+        self.e3 = nn.Linear(1300, 1000)
+        self.e4 = nn.Linear(1000, 700)
+        self.e5 = nn.Linear(700, 400)
         # Latent View
-        self.lv = nn.Linear(250, 100)
+        self.lv = nn.Linear(400, 100)
         # Decoder
-        self.d1 = nn.Linear(100, 250)
-        self.d2 = nn.Linear(250, 500)
-        self.d3 = nn.Linear(500, 1000)
-        self.output_layer = nn.Linear(1000, 2250)
+        self.d1 = nn.Linear(100, 400)
+        self.d2 = nn.Linear(400, 700)
+        self.d3 = nn.Linear(700, 1000)
+        self.d4 = nn.Linear(1000, 1300)
+        self.d5 = nn.Linear(1300, 1600)
+        self.output_layer = nn.Linear(1600, 1900)
         
     def forward(self,x):
         x = F.relu(self.e1(x))
         x = F.relu(self.e2(x))
         x = F.relu(self.e3(x))
+        x = F.relu(self.e4(x))
+        x = F.relu(self.e5(x))
         x = torch.sigmoid(self.lv(x))
         x = F.relu(self.d1(x))
         x = F.relu(self.d2(x))
         x = F.relu(self.d3(x))
+        x = F.relu(self.d4(x))
+        x = F.relu(self.d5(x))
         x = self.output_layer(x)
         return x
 
-def train(dataloader, model, optimizer, loss_func, batch_size):
+def train(dataloader, model, optimizer, loss_func, epoch):
     model.train()
     train_loss = 0
-    EPOCHS = 100
 
-    for epoch in range(EPOCHS):
-        epoch_start = timeit.default_timer()
-        for index, (data, ) in enumerate(dataloader, 1):
-            optimizer.zero_grad()
-            pred = model(data)
-            loss = loss_func(pred, data)
-            train_loss += loss.item()
-            loss.backward() # backpropagation
-            optimizer.step()
-            print('\repoch %2d [%3d/%3d] train_loss %5.3f' % (epoch, index, len(dataloader), loss.item()), end='')
-        print(' time %5.2f' % (timeit.default_timer() - epoch_start))
+    for index, (data, ) in enumerate(dataloader, 1):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_func(output, data)
+        train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        print('\repoch %2d [%3d/%3d] train_loss %5.3f' % (epoch, index, len(dataloader), train_loss / index), end='')
 
-def test(dataloader, model):
+def test(dataloader, model, loss_func):
     model.eval()
-    predictions = []
-    for data, in dataloader:
-            pred = model(data)
-            for prediction in pred:
-                predictions.append(prediction)
-    return predictions
+    test_loss = 0
+
+    for index, (data, ) in enumerate(dataloader, 1):
+        with torch.no_grad():
+            output = model(data)
+        loss = loss_func(output, data)
+        test_loss += loss.item()
+    print(' test_loss %5.3f' % (test_loss / index), end='')
 
 def main():
+    epochs = 1000
     batch_size = 100
 
     if len(sys.argv) < 2:
@@ -104,10 +111,13 @@ def main():
     loss_func = nn.MSELoss()
     optimizer = torch.optim.Adam(ae.parameters(), lr=1e-4)
 
-    train(train_dataloader, ae, optimizer, loss_func, batch_size)
+    for epoch in range(epochs):
+        epoch_start = timeit.default_timer()
 
-    predictions = test(test_dataloader, ae)
-    print(len(predictions))
+        train(train_dataloader, ae, optimizer, loss_func, epoch)
+        test(test_dataloader, ae, loss_func)
+
+        print(' time %5.2f' % (timeit.default_timer() - epoch_start))
 
 if __name__ == '__main__':
     main()
