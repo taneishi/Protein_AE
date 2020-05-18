5d8ff3ddf367f84db6fea21da9bf592b10a04f27
taneishi <taneishi@users.noreply.github.com>
2020-05-18 11:17:34 +0900

    separted model code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data
+import argparse
 import timeit
 import sys
 import os
+
+from model import AutoEncoder
 
 def load_dataset(filename, batch_size, device):
     # Load dataset
@@ -26,47 +28,12 @@ def load_dataset(filename, batch_size, device):
     train = torch.utils.data.TensorDataset(train_x_torch)
     test = torch.utils.data.TensorDataset(test_x_torch)
 
-    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
-    test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True)
 
     return train_dataloader, test_dataloader
 
-class AutoEncoder(nn.Module):
-    
-    def __init__(self):
-        super(AutoEncoder, self).__init__()
-        # encoder
-        self.e1 = nn.Linear(1900, 1600)
-        self.e2 = nn.Linear(1600, 1300)
-        self.e3 = nn.Linear(1300, 1000)
-        self.e4 = nn.Linear(1000, 700)
-        self.e5 = nn.Linear(700, 400)
-        # Latent View
-        self.lv = nn.Linear(400, 100)
-        # Decoder
-        self.d1 = nn.Linear(100, 400)
-        self.d2 = nn.Linear(400, 700)
-        self.d3 = nn.Linear(700, 1000)
-        self.d4 = nn.Linear(1000, 1300)
-        self.d5 = nn.Linear(1300, 1600)
-        self.output_layer = nn.Linear(1600, 1900)
-        
-    def forward(self,x):
-        x = F.relu(self.e1(x))
-        x = F.relu(self.e2(x))
-        x = F.relu(self.e3(x))
-        x = F.relu(self.e4(x))
-        x = F.relu(self.e5(x))
-        x = torch.sigmoid(self.lv(x))
-        x = F.relu(self.d1(x))
-        x = F.relu(self.d2(x))
-        x = F.relu(self.d3(x))
-        x = F.relu(self.d4(x))
-        x = F.relu(self.d5(x))
-        x = self.output_layer(x)
-        return x
-
-def train(dataloader, model, optimizer, loss_func):
+def train(dataloader, model, optimizer, loss_func, epoch):
     model.train()
     train_loss = 0
 
@@ -77,7 +44,8 @@ def train(dataloader, model, optimizer, loss_func):
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
-        print('\rbatch [%3d/%3d] train_loss %6.3f' % (index, len(dataloader), train_loss / index), end='')
+
+        print('\repoch %4d batch [%3d/%3d] train_loss %6.3f' % (epoch, index, len(dataloader), train_loss / index), end='')
 
     return train_loss / index
 
@@ -90,30 +58,26 @@ def test(dataloader, model, loss_func):
             output = model(data)
         loss = loss_func(output, data)
         test_loss += loss.item()
+
     print(' test_loss %6.3f' % (test_loss / index), end='')
 
     return test_loss / index
 
-def main():
+def main(args):
     epochs = 1000
     batch_size = 100
-
-    if len(sys.argv) < 2:
-        sys.exit('no input filename')
-    else:
-        filename = sys.argv[1]
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using %s device.' % device)
 
-    train_dataloader, test_dataloader = load_dataset(filename, batch_size, device)
+    train_dataloader, test_dataloader = load_dataset(args.filename, batch_size, device)
 
     model = AutoEncoder().to(device)
     print(model)
 
     # define our optimizer and loss function
-    loss_func = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    loss_func = nn.MSELoss()
 
     train_losses = [np.inf]
     test_losses = [np.inf]
@@ -121,16 +85,16 @@ def main():
     for epoch in range(epochs):
         epoch_start = timeit.default_timer()
 
-        train_loss = train(train_dataloader, model, optimizer, loss_func)
+        train_loss = train(train_dataloader, model, optimizer, loss_func, epoch)
         test_loss = test(test_dataloader, model, loss_func)
 
-        print(' epoch [%4d/%4d] time %5.2f' % (epoch, epochs, (timeit.default_timer() - epoch_start)), end='')
+        print(' %5.2f sec' % (timeit.default_timer() - epoch_start), end='')
 
         train_losses.append(train_loss)
         test_losses.append(test_loss)
 
         if test_loss < min(test_losses[:-1]):
-            torch.save(model.state_dict(), 'model.pth' % epoch)
+            torch.save(model.state_dict(), 'model.pth')
             print(' model updated')
         else:
             print('')
@@ -139,4 +103,8 @@ def main():
             break
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    main(args)
---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AutoEncoder(nn.Module):
+    
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        # encoder
+        self.e1 = nn.Linear(1900, 1600)
+        self.e2 = nn.Linear(1600, 1300)
+        self.e3 = nn.Linear(1300, 1000)
+        self.e4 = nn.Linear(1000, 700)
+        self.e5 = nn.Linear(700, 400)
+        # Latent View
+        self.lv = nn.Linear(400, 100)
+        # Decoder
+        self.d1 = nn.Linear(100, 400)
+        self.d2 = nn.Linear(400, 700)
+        self.d3 = nn.Linear(700, 1000)
+        self.d4 = nn.Linear(1000, 1300)
+        self.d5 = nn.Linear(1300, 1600)
+        self.output_layer = nn.Linear(1600, 1900)
+        
+    def forward(self,x):
+        x = F.relu(self.e1(x))
+        x = F.relu(self.e2(x))
+        x = F.relu(self.e3(x))
+        x = F.relu(self.e4(x))
+        x = F.relu(self.e5(x))
+        x = torch.sigmoid(self.lv(x))
+        x = F.relu(self.d1(x))
+        x = F.relu(self.d2(x))
+        x = F.relu(self.d3(x))
+        x = F.relu(self.d4(x))
+        x = F.relu(self.d5(x))
+        x = self.output_layer(x)
+        return x
